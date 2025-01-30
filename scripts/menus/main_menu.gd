9f01d3bba8bb775d4f392c90bd45546cacf76565
CzartB1 <113404547+CzartB1@users.noreply.github.com>
2025-01-30 22:03:40 +0700

    Main Menu Functionalities

--- scripts/menus/main_menu.gd
+++ scripts/menus/main_menu.gd
@@ -1,9 +1,25 @@
-extends Control
+extends CanvasLayer
 
 @export var game_scene:PackedScene
+@export var anim:AnimationPlayer
+@export var settingsMenu:Control
+var pressed=false
+
+func _ready():
+	settingsMenu.visible=false
+	settingsMenu.z_index=0
+
+func _process(_delta):
+	if !pressed and Input.is_anything_pressed():
+		anim.play("title_screen_press")
+		pressed=true
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scene/test_world.tscn")
 
 func _on_exit_pressed():
 	get_tree().quit()
+
+func _on_settings_pressed():
+	settingsMenu.visible=true
+	settingsMenu.z_index=7
