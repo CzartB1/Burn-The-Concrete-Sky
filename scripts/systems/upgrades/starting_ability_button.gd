6f41cb878d2eb63f69f441f4a50cb0e7a8054e8e
CzartB1 <czartb1@gmail.com>
2025-03-05 13:58:51 +0700

    gift menu visuals

--- scripts/systems/upgrades/starting_ability_button.gd
+++ scripts/systems/upgrades/starting_ability_button.gd
@@ -5,11 +5,13 @@ var plr:Player
 var start_screen:Starting_Weapon_Screen
 @export var ability_id:int
 @export var icon_:TextureRect
+@export var label:RichTextLabel
+@export var descriptor:RichTextLabel
 
 func _process(delta):
 	if  plr == null:
 		plr = get_tree().get_first_node_in_group("Player")
-	#text=str(plr.upgrade_manager.available_upgrade[ability_id].name)
+	label.text="[center]"+str(plr.upgrade_manager.available_upgrade[ability_id].name)
 	icon_.texture=plr.upgrade_manager.available_upgrade[ability_id].icon
 
 func _on_pressed():
@@ -17,3 +19,4 @@ func _on_pressed():
 	start_screen.selected=true
 	#print(str(plr.upgrade_manager.equipped_upgrade))
 	start_screen.weapon=false
+	descriptor.text="[center]"+plr.upgrade_manager.available_upgrade[ability_id].Description
--- scripts/systems/upgrades/starting_weapon_button.gd
+++ scripts/systems/upgrades/starting_weapon_button.gd
@@ -3,13 +3,15 @@ extends Button
 
 var plr:Player
 @export var icon_:TextureRect
+@export var label:RichTextLabel
+@export var descriptor:RichTextLabel
 var start_screen:Starting_Weapon_Screen
 var weapon_id:int
 
 func _process(delta):
 	if  plr == null:
 		plr = get_tree().get_first_node_in_group("Player")
-	#text=str(plr.upgrade_manager.weapon_manager.available_weapons[weapon_id].name)
+	label.text="[center]"+str(plr.upgrade_manager.weapon_manager.available_weapons[weapon_id].name)
 	icon_.texture=plr.upgrade_manager.weapon_manager.available_weapons[weapon_id].icon
 
 func _on_pressed():
@@ -17,3 +19,4 @@ func _on_pressed():
 	start_screen.selected=true
 	#print(str(plr.upgrade_manager.equipped_upgrade))
 	start_screen.weapon=true
+	descriptor.text="[center]"+plr.upgrade_manager.weapon_manager.available_weapons[weapon_id].Description
