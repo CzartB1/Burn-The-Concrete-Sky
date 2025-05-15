c92be0ce8c620b27a60d9dc2f8047350d073b2b7
CzartB1 <113404547+CzartB1@users.noreply.github.com>
2025-05-15 20:41:23 +0700

    melee weapon

--- scripts/player/weapon/melee_weapon.gd
+++ scripts/player/weapon/melee_weapon.gd
@@ -1,7 +1,6 @@
 class_name melee_weapon
-extends Node3D
+extends BaseWeapon
 
-@export var manager: Weapon_Manager
 @export var hitbox: Area3D
 @export var visual_effects: Node3D
 @export var deflect_bullet:bool=false
@@ -24,11 +23,8 @@ var damage: int
 @export var icon:AtlasTexture
 @export_multiline var Description:String
 @export_group("audio")
-var audio_direct=preload("res://scene/utilities/audio_direct.tscn")
 @export var attack_sound:AudioStream
-var charging=false
 var attacking: bool = false
-var can_attack=true
 
 func _physics_process(delta):
 	if attacking:
@@ -51,43 +47,40 @@ func _physics_process(delta):
 		visual_effects.visible = false
 
 func _process(delta):
+	super._process(delta)
 	manager.master.anim_manager.set("parameters/weapon_anim_id/blend_position",float(stance))
-	
-	attack()
 
-func attack():
-	if !manager.master.alive and manager.master.disabled and game_manager.paused and manager.master.in_dialogue: return
-	if can_attack and Input.is_action_just_pressed("attack") and !manager.master.in_dialogue:
+func attack_press():
+	manager.master.charge_bar.max_value=max_damage
+	charge_start=Time.get_ticks_msec()/1000.0
+
+func attack_hold():
+	if !holding:
 		manager.master.charge_bar.max_value=max_damage
 		charge_start=Time.get_ticks_msec()/1000.0
-		charging=true
-	if can_attack and Input.is_action_pressed("attack") and !manager.master.in_dialogue:
-		if !charging:
-			manager.master.charge_bar.max_value=max_damage
-			charge_start=Time.get_ticks_msec()/1000.0
-			charging=true
-		elif charging:
-			charge_dur=clamp(Time.get_ticks_msec()/1000.0-charge_start,0,3)
-			manager.master.charge_bar.value=min_damage*(max_damage-min_damage)*charge_dur
-	if can_attack and Input.is_action_just_released("attack") and charging and !manager.master.in_dialogue:
-		var dec=get_tree().get_first_node_in_group("Decoy")
-		if dec!=null and dec is Kunoichi_Decoy: dec.dead()
-		
-		play_sound(attack_sound)
-		manager.master.charge_bar.value=0
-		charge_dur=clamp(Time.get_ticks_msec()/1000.0-charge_start,0,3) #FIX ME sometimes crashes the game. maybe bcs it activated when another function thats needed isn't ready yet.
-		damage=min_damage*(max_damage-min_damage)*charge_dur
-		if damage > max_damage: damage=max_damage
-		elif damage < min_damage: damage=min_damage
-		print("final damage: "+str(damage))
-		manager.master.anim_manager.set("parameters/melee_attack_type/blend_position",float(strike_type))
-		manager.master.anim_manager.set("parameters/melee_attack/request",AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
-		charging=false
-		attacking = true
-		await get_tree().create_timer(0.1).timeout
-		attacking = false
-		can_attack=false
-		cooldown_timer.start()
+	elif holding:
+		charge_dur=clamp(Time.get_ticks_msec()/1000.0-charge_start,0,3)
+		manager.master.charge_bar.value=min_damage*(max_damage-min_damage)*charge_dur
+
+func attack_released():
+	if !charge_start: return
+	var dec=get_tree().get_first_node_in_group("Decoy")
+	if dec!=null and dec is Kunoichi_Decoy: dec.dead()
+	
+	play_sound(attack_sound)
+	manager.master.charge_bar.value=0
+	charge_dur=clamp(Time.get_ticks_msec()/1000-charge_start,0,3) #FIXME sometimes crashes the game. maybe bcs it activated when another function thats needed isn't ready yet.
+	damage=min_damage*(max_damage-min_damage)*charge_dur
+	if damage > max_damage: damage=max_damage
+	elif damage < min_damage: damage=min_damage
+	print("final damage: "+str(damage))
+	manager.master.anim_manager.set("parameters/melee_attack_type/blend_position",float(strike_type))
+	manager.master.anim_manager.set("parameters/melee_attack/request",AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+	attacking = true
+	await get_tree().create_timer(0.1).timeout
+	attacking = false
+	can_attack=false
+	cooldown_timer.start()
 
 func _on_cooldown_timeout():
 	ignore_bodies.clear()
