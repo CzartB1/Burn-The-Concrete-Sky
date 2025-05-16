a76c04a5effae16bf233de3ff2476780ca75a89b
CzartB1 <czartb1@gmail.com>
2025-05-16 10:27:32 +0700

    simple weapon tagging

--- scripts/player/weapon/weapon.gd
+++ scripts/player/weapon/weapon.gd
@@ -2,6 +2,7 @@ class_name BaseWeapon
 extends Node3D
 
 @export var manager: Weapon_Manager
+@export var tags: Array[String] = []
 var audio_direct=preload("res://scene/utilities/audio_direct.tscn")
 var holding=false
 var can_attack=true
@@ -12,15 +13,15 @@ var can_attack=true
 @export_multiline var Description:String
 
 func _process(_delta):
-	if !manager.master.alive and manager.master.disabled and game_manager.paused and manager.master.in_dialogue: return
-	if can_attack and Input.is_action_just_pressed("attack"):
-		attack_press()
-	if can_attack and Input.is_action_pressed("attack"):
-		holding=true
-		attack_hold()
-	if can_attack and Input.is_action_just_released("attack"):
-		attack_released()
-		holding=false
+	if manager.master.alive and !manager.master.disabled and !game_manager.paused and !manager.master.in_dialogue:
+		if can_attack and Input.is_action_just_pressed("attack"):
+			attack_press()
+		if can_attack and Input.is_action_pressed("attack"):
+			holding=true
+			attack_hold()
+		if can_attack and Input.is_action_just_released("attack"):
+			attack_released()
+			holding=false
 
 func attack_press():
 	pass
@@ -38,3 +39,12 @@ func play_sound(austr:AudioStream):
 		audio.play_sound(austr)
 	elif !austr:
 		print("sound not found")
+
+func has_tag(tag: String) -> bool:
+	return tag in tags
+
+func get_tags() -> String:
+	var result := ""
+	for tag in tags:
+		result += "[ " + tag.capitalize() + " ] "
+	return result.strip_edges()
--- scripts/systems/upgrades/weapon_button.gd
+++ scripts/systems/upgrades/weapon_button.gd
@@ -6,6 +6,7 @@ var plr:Player
 @export var weapon_id:int
 @export var button_enable:Control
 @export var button_disable:Control
+@export var tag_box:RichTextLabel
 @export var description_box:RichTextLabel
 var w_object
 var weapon:Node3D
@@ -15,15 +16,11 @@ func _process(delta):
 	if plr==null: plr=get_tree().get_first_node_in_group("Player")
 	if w_object!=null:
 		text=w_object.name + " - " + str(w_object.price)
-	# FIXME_ disable disable when not choosing this weapon
-	#if weapon_screen.selected_weapon_id!=null:
-		#if weapon_screen.selected_weapon_id!=plr.upgrade_manager.weapon_manager.available_weapons.find(weapon):
-			#disabled=false
 
 func _on_pressed():
-	#if plr!=null:
 	if w_object!=null:
 		description_box.text=w_object.Description
+		tag_box.text=w_object.get_tags()
 	weapon_screen.selected_weapon_id=plr.upgrade_manager.weapon_manager.available_weapons.find(weapon)
 	weapon_screen.selected=true
 	print(str(plr.upgrade_manager.equipped_upgrade))
