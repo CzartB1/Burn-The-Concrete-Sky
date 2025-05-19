51241f9174da0d5b2899187f95eaf5e7ea038b34
CzartB1 <czartb1@gmail.com>
2025-05-19 13:00:02 +0700

    Update see_no_evil.gd

--- scripts/player/upgrades/see_no_evil.gd
+++ scripts/player/upgrades/see_no_evil.gd
@@ -12,4 +12,6 @@ func _process(delta):
 			vignette.visible=true
 			manager.player.move_speed_multiplier += .2
 			manager.player.dash_speed_multiplier += .2
+			var wh=get_tree().get_first_node_in_group("weapon_holder")
+			if wh is Weapon_Manager: wh.damage_multiplier+=1
 			a = true
